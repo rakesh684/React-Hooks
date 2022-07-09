7e1010ed3f903b2bc0a9bfaa1e08c52f8a1c20ec
Rakesh kumar <dr.rakesh684@gmail.com>
2022-07-10 02:03:40 +0530

    React Hooks useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React ,{useState} from "react"
 import './App.css';
+// class App extends React.Component {
+//   constructor(){
+//     super()
+//     this.state={
+//       answer:"Yes"
+//     }
+//   }
+//   render(){
+//     return (
+//       <div className="App">
+//        <h1>{`${this.state.answer}, Rakesh is doing good`}</h1>
+//       </div>
+//     );
+//   }
+// }
+// converting above class component into functional component
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  const [inputData,setInputData]=useState(
+    {
+      firstName:"",
+      lastName:""
+    }
+  )
+  const [contactData,setContactData]=useState([])
+  function handleChange(event){
+    const {name,value}=event.target
+    setInputData(previnputData=>(
+     { ...previnputData,
+      [name]:value}
+    ))
+    
+  }
+  console.log(inputData)
+  function handleSubmit(event){
+     event.preventDefault()
+     setContactData(prevContactData=>[...prevContactData,inputData])
+     
+  }
+  const contacts=contactData.map(contact=>
+    <h2 key={contact.firstName + contact.lastName}>
+     {contact.firstName} {contact.lastName}</h2>)
+  return(
+    <>
+      <form>
+         <input 
+            placeholder="First Name"
+            name="firstName"
+            value={inputData.firstName}
+            onChange={handleChange}
+          />
+          <input 
+            placeholder="Last Name"
+            name="lastName"
+            value={inputData.lastName}
+            onChange={handleChange}
+          />
+          <br/>
+          <button onClick={handleSubmit} >Button</button>
+      </form>
+      {contacts}
+    </>
+  )
 }
 
 export default App;
